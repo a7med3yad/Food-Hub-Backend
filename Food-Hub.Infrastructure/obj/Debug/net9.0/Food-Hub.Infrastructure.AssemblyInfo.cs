9e72830887044ad891d6f1c938c30578ff76cea3
Ahmed Ayad <ahmed.ibrahim01974@gmail.com>
2025-11-27 22:48:21 +0200

    a

--- Food-Hub.Infrastructure/obj/Debug/net9.0/Food-Hub.Infrastructure.AssemblyInfo.cs
+++ Food-Hub.Infrastructure/obj/Debug/net9.0/Food-Hub.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Food-Hub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dca4019d255cef9210f835506d95674f164eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdcc46bb2fb830a2b0d372b7b7f510db905942be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Food-Hub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Food-Hub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
